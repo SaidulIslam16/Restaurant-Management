89a1767a7271493b3ef30a54457dc845f805b8aa
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-11-29 10:36:28 +0600

    restaurant managemtn app setup for development

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello from Resturant React app</h1>
+      <button className="btn btn-accent">Hello Button</button>
+
+    </>
+  )
+}
+
+export default App
