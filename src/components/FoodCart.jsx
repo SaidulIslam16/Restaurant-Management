a41916f206970417415cc972b59cb9fbbdaeae1f
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-12-02 12:13:58 +0600

    cart data added to database

--- src/components/FoodCart.jsx
+++ src/components/FoodCart.jsx
@@ -1,18 +1,26 @@
 import { useContext } from "react";
 import { AuthContext } from "../providers/AuthProvider";
 import Swal from "sweetalert2";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const FoodCart = ({ item }) => {
 
-    const { name, image, price, recipe } = item;
+    const { name, image, price, recipe, _id } = item;
     const { user } = useContext(AuthContext);
     const navigate = useNavigate();
+    const location = useLocation();
 
     const handleAddToCart = item => {
-        console.log(item);
-        if (user) {
-            fetch('http://localhost:5000/carts')
+        // console.log(item);
+        if (user && user.email) {
+            const cartItem = { menuItemId: _id, name, image, price, recipe, email: user.email };
+            fetch('http://localhost:5000/carts', {
+                method: 'POST',
+                headers: {
+                    'content-type': 'application/json'
+                },
+                body: JSON.stringify(cartItem)
+            })
                 .then(res => res.json())
                 .then(data => {
                     if (data.insertedId) {
@@ -24,22 +32,23 @@ const FoodCart = ({ item }) => {
                             timer: 1500
                         });
                     }
-                    else {
-                        Swal.fire({
-                            title: "Are you sure?",
-                            text: "You won't be able to revert this!",
-                            icon: "warning",
-                            showCancelButton: true,
-                            confirmButtonColor: "#3085d6",
-                            cancelButtonColor: "#d33",
-                            confirmButtonText: "Please Login"
-                        }).then((result) => {
-                            if (result.isConfirmed) {
-                                navigate('/login')
-                            }
-                        });
-                    }
+
                 })
+        }
+        else {
+            Swal.fire({
+                title: "Are you sure?",
+                text: "Please Login to Add to Cart",
+                icon: "warning",
+                showCancelButton: true,
+                confirmButtonColor: "#3085d6",
+                cancelButtonColor: "#d33",
+                confirmButtonText: "Login Now"
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    navigate('/login', { state: { from: location } });
+                }
+            });
         }
     }
 
