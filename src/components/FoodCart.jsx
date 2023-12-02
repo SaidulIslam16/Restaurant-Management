f280d71bebb6841ad0e88ef267be0c861d9869bf
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-12-02 11:51:23 +0600

    alart added for add to cart confirmation

--- src/components/FoodCart.jsx
+++ src/components/FoodCart.jsx
@@ -1,9 +1,46 @@
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
 const FoodCart = ({ item }) => {
 
     const { name, image, price, recipe } = item;
+    const { user } = useContext(AuthContext);
+    const navigate = useNavigate();
 
     const handleAddToCart = item => {
         console.log(item);
+        if (user) {
+            fetch('http://localhost:5000/carts')
+                .then(res => res.json())
+                .then(data => {
+                    if (data.insertedId) {
+                        Swal.fire({
+                            position: "top-end",
+                            icon: "success",
+                            title: "Item Added to Cart",
+                            showConfirmButton: false,
+                            timer: 1500
+                        });
+                    }
+                    else {
+                        Swal.fire({
+                            title: "Are you sure?",
+                            text: "You won't be able to revert this!",
+                            icon: "warning",
+                            showCancelButton: true,
+                            confirmButtonColor: "#3085d6",
+                            cancelButtonColor: "#d33",
+                            confirmButtonText: "Please Login"
+                        }).then((result) => {
+                            if (result.isConfirmed) {
+                                navigate('/login')
+                            }
+                        });
+                    }
+                })
+        }
     }
 
     return (
